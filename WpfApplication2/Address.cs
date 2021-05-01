e19822d2aa9ee69d095b09d4881fe66b39857cc6
Xoder <pw333t@gmail.com>
2021-05-01 21:51:35 +0300

    First commit

---
+++ WpfApplication2/Address.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfApplication2.Annotations;
+
+namespace WpfApplication2
+{
+    public class Address : INotifyPropertyChanged
+    {
+        private string status;
+        private long time;
+        public string IpAdd { get; set; }
+        public string Name { get; set; }
+
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+                OnPropertyChanged();
+                
+            }
+        }
+
+        public long Time
+        {
+            get => time;
+            set
+            {
+                time = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public Address(string ip, string name)
+        {
+            Name = name;
+            IpAdd = ip;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        //private ObservableCollection<Address> ipAddresses = new ObservableCollection<Address>();
+        public ObservableCollection<Address> ipAddresses { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ipAddresses = new ObservableCollection<Address>();
+            ipAddresses.Add(new Address("192.168.9.1","Микрота"));
+            ipAddresses.Add(new Address("8.8.8.8","Google"));
+            ipAddresses.Add(new Address("192.168.9.236","Ноут"));
+
+            lv_data.ItemsSource = ipAddresses;
+        }
+
+        private void Ping_button_OnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in ipAddresses)
+            {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send(item.IpAdd);
+                item.Status = Convert.ToString(reply.Status);
+                if (Convert.ToString(item.Status) == "Success")
+                {
+                    item.Time = Convert.ToInt64(reply.RoundtripTime);
+                }
+            }
+        }
+    }
+}
